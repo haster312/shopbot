78cf31a833edaeb65933a7872e7b6612dcfa3025
Vu Tuan Anh <vu@Vus-MBP-2.lan>
2016-11-24 20:24:28 +0700

    commit

--- public/update.php
+++ public/update.php
@@ -3,9 +3,9 @@ use GigaAI\Http\ThreadSettings;
 //commands to reload bot when a new source version is deployed to heroku
 
 //Update threadSettings
-GigaAI\Http\ThreadSettings::updateGetStartedButton();
-GigaAI\Http\ThreadSettings::updateGreetingText();
-GigaAI\Http\ThreadSettings::updatePersistentMenu();
+ThreadSettings::updateGetStartedButton();
+ThreadSettings::updateGreetingText();
+ThreadSettings::updatePersistentMenu();
 
 //Update seeder
 require_once 'seeder.php';
