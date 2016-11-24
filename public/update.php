1cfe91fd610f9e1b5a28eb692d31c4168bbef2c5
Vu Tuan Anh <vu@Vus-MBP-2.lan>
2016-11-24 20:44:10 +0700

    commit

--- public/update.php
+++ public/update.php
@@ -3,7 +3,7 @@ $bot = require_once __DIR__ . '/../bootstrap/bot.php';
 //commands to reload bot when a new source version is deployed to heroku
 
 //Update threadSettings
-GigaAI\Http\ThreadSettings::updateGetStartedButton();
+GigaAI\Http\ThreadSettings::updateGetStartedButtonsdsd();
 GigaAI\Http\ThreadSettings::updateGreetingText();
 GigaAI\Http\ThreadSettings::updatePersistentMenu();
 
