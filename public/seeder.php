f16fa082001acdc967230b9f723a760500056304
Vu Tuan Anh <vu@Vus-MacBook-Pro-2.local>
2016-11-25 11:58:59 +0700

    cm

--- public/seeder.php
+++ public/seeder.php
@@ -51,31 +51,63 @@ $bot->answer('payload:USER_TAPPED_EVENT', function() {
 $bot->answer('payload:USER_TAPPED_PRODUCT', 'Please let me know the product line which you interested in')
 	->wait('category');
 $bot->answer('@category', [
-        [
-            "title"     => "Iphone",
-            "image_url" => "https://support.apple.com/library/content/dam/edam/applecare/images/en_US/iphone/iphone-6s-colors.jpg",
-            "subtitle"  => "The iPhones",
-            "buttons"   => [
-                [
-                    "type"      => "payload",
-                    "payload"   => "USER_CLICKED_DETAILED_BUTTON",
-                    "title"     => "Detail"
-                ]
-            ]
-        ],
-        [
-            "title"     => "Iphone",
-            "image_url" => "https://support.apple.com/library/content/dam/edam/applecare/images/en_US/iphone/iphone-6s-colors.jpg",
-            "subtitle"  => "The iPhones",
-            "buttons"   => [
-                [
-                    "type"      => "payload",
-                    "payload"   => "USER_CLICKED_DETAILED_BUTTON",
-                    "title"     => "Detail"
-                ]
+    // A generic bubble
+    [
+        "title"     => "Lamborghini",
+        "image_url" => "http://pictures.topspeed.com/IMG/crop/201603/2016-lamborghini-centenar-5_800x0w.jpg",
+        "subtitle"  => "Amazing speed",
+        "buttons"   => [
+            [
+                "type"  => "web_url",
+                "url"   => "https://lamborghini.com",
+                "title" => "Buy Now"
+            ],
+            [
+                "type"    => "postback",
+                "payload" => "BUY_LAMBORGHINI_TOMORROW",
+                "title"   => "Buy Tomorrow"
             ]
         ]
-    ]);
+    ],
+
+    // Another generic bubble
+    [
+        "title"     => "Rolls Royce",
+        "image_url" => "http://static.robbreport.com/sites/default/files/images/articles/2015Sep/1642581//rolls-royce-dawn-02.jpg",
+        "subtitle"  => "Most Luxury Car",
+        "buttons"   => [
+            [
+                "type"  => "web_url",
+                "url"   => "https://www.rolls-roycemotorcars.com/en-GB/home.html",
+                "title" => "Buy Now"
+            ],
+            [
+                "type"    => "postback",
+                "payload" => "BUY_ROLLSROYCE_TOMORROW",
+                "title"   => "Buy Tomorrow"
+            ]
+        ]
+    ],
+
+    // Yet another generic bubble
+    [
+        "title"     => "Ferrari",
+        "image_url" => "https://cdn1.vox-cdn.com/thumbor/-pG8Dcb_qtRf6te3ug12FHhqUDs=/1020x0/cdn0.vox-cdn.com/uploads/chorus_asset/file/4156848/Ferrari_F12tdf_3low.0.jpg",
+        "subtitle"  => "I like the horse",
+        "buttons"   => [
+            [
+                "type"  => "web_url",
+                "url"   => "http://www.ferrari.com/en_en/",
+                "title" => "Buy Now"
+            ],
+            [
+                "type"    => "postback",
+                "payload" => "BUY_FERRARI_TOMORROW",
+                "title"   => "Buy Tomorrow"
+            ]
+        ]
+    ]
+]);
 
 // Help
 $bot->answer('payload:USER_TAPPED_ABOUT', [
