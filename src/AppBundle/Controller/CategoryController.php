e66cb2932401270f3a9f1a5c5aa6c1c86e5ae3e9
Hoang Nguyen <hoangnn1@asiantech.vn>
2016-11-26 10:44:09 +0700

    Fix

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,6 +11,6 @@ class CategoryController extends Controller {
     public function allCategoryAction() {
         $categories = $this->get('category.business')->getAllCategory();
         var_dump($categories);
-        return $categories;
+        return new JsonResponse($categories);
     }
 }
