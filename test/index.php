89ade1c80300f142ad46948b7b0edd6b96d49981
Hoang Nguyen <hoangnn1@asiantech.vn>
2016-11-27 00:32:09 +0700

    Fix

--- test/index.php
+++ test/index.php
@@ -3,4 +3,6 @@ $bot = require_once __DIR__ . '/../bootstrap/bot.php';
 use Api\Business\CategoryBusiness;
 
 $categories = CategoryBusiness::getAllCategories(1,0);
+$product = CategoryBusiness::getProducts(1);
 var_dump($categories);
+var_dump($product);
