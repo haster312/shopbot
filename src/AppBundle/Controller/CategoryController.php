4ff6c34a231fb2e1cd97a31e2849fad1ed20c113
Hoang Nguyen <hoangnn1@asiantech.vn>
2016-11-26 10:26:04 +0700

    Fix

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -9,7 +9,7 @@ class CategoryController extends Controller {
 
     public function allCategoryAction() {
         $categories = $this->get('category.business')->getAllCategory();
-        var_dump($categories);exit;
+        var_dump($categories);
         return $categories;
     }
 }
