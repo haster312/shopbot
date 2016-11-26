ba745a0b5936e07093aaee9fa3dbeb208d1b934a
Hoang Nguyen <hoangnn1@asiantech.vn>
2016-11-27 00:17:16 +0700

    Fix

--- Api/Business/CategoryBusiness.php
+++ Api/Business/CategoryBusiness.php
@@ -8,16 +8,15 @@ class CategoryBusiness extends Business
     public static function getAllCategories($length = null, $page = null){
         new static;
 
-        $categories = new Category();
-        $categories->select('*');
-        if($length > 0) {
-            $categories->limit($length);
-        }
-
-        if($length > 0 && $page > 0) {
-            $offset = $length * $page;
-            $categories->offset($offset);
-        }
+        $categories = Category::select('*');
+//        if($length > 0) {
+//            $categories->limit($length);
+//        }
+//
+//        if($length > 0 && $page > 0) {
+//            $offset = $length * $page;
+//            $categories->offset($offset);
+//        }
         return $categories->get();
     }
 }
