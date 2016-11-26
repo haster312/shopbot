72070895a399a831e05967d355041c1086b2c73b
Hoang Nguyen <hoangnn1@asiantech.vn>
2016-11-26 10:30:01 +0700

    Fix

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -3,13 +3,13 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Business\CategoryBusiness;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller {
 
     public function allCategoryAction() {
         $categories = $this->get('category.business')->getAllCategory();
         var_dump($categories);
-        return $categories;
+        return new Response($categories);
     }
 }
