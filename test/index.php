2f3353908ff1cb182d9725eedbef65d23e413a50
Hoang Nguyen <hoangnn1@asiantech.vn>
2016-11-27 20:31:27 +0700

    Fix

--- test/index.php
+++ test/index.php
@@ -1,9 +1,13 @@
 <?php
 $bot = require_once __DIR__ . '/../bootstrap/bot.php';
-use Api\Business\ProductOrderBusiness;
-
-$order = ProductOrderBusiness::createOrder("10206973580408350", 8);
-
-var_dump($order);
+use Api\Business\LeadBusiness;
+$lead['firstname'] = "Hoàng";
+$lead['lastname']  = "Nguyễn";
+$lead['facebook_id'] = "100";
+$lead['phone'] = "0909620069";
+$lead['email'] = "sallyminiv1@gmail.com";
+$lead['street'] = "Cù Chính Lan";
+$lead = LeadBusiness::createLead($lead);
+var_dump($lead);
 
 
