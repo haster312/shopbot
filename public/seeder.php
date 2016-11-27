098f36e406e66525da13774c3a1139e2f019ffab
Vu Tuan Anh <vu@Vus-MacBook-Pro-2.local>
2016-11-27 14:56:05 +0700

    commit

--- public/seeder.php
+++ public/seeder.php
@@ -128,7 +128,7 @@ $bot->answer('payload:cat_%', function($bot, $lead_id, $input){
 */
 $productId = '';
 $lead = new \Api\Model\Lead();
-$bot->answer('payload:order_%', function($bot, $lead_id, $input) use (&$productId){
+$bot->answer('payload: order_%', function($bot, $lead_id, $input) use (&$productId){
     //Get product id
     $payload = $bot->received->entry[0]->messaging[0]->postback->payload;
     $productId = explode('_', $payload)[1];
