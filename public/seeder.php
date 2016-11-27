4d97a86d0289f85656c8c7a40724684fb46cfb3b
Vu Tuan Anh <vu@Vus-MacBook-Pro-2.local>
2016-11-28 01:17:43 +0700

    commit

--- public/seeder.php
+++ public/seeder.php
@@ -185,24 +185,53 @@ $bot->answer('Receipt', 'Please let me know your order id')->then(function($bot,
         return 'It seem not a valid order id, could you check and enter again?';
     return
         [
-            "recipient_name" => $userProfile['first_name'] . ' ' . $userProfile['last_name'],
-            "order_number" => $receipt['ordernumber__c'],
+            "recipient_name" => "Stephane Crozatier",
+            "order_number" => rand(0, 100000),
             "currency" => "USD",
+            "payment_method" => "Visa 2345",
+            "order_url" => "http://petersapparel.parseapp.com/order?order_id=123456",
+            "timestamp" => "1428444852",
             "elements" => [
                 [
                     "title" => "Classic White T-Shirt",
                     "subtitle" => "100% Soft and Luxurious Cotton",
-                    "quantity" => 1,
-                    "price" => $receipt['totalamount__c'],
+                    "quantity" => 2,
+                    "price" => 50,
                     "currency" => "USD",
                     "image_url" => "http://petersapparel.parseapp.com/img/whiteshirt.png"
+                ],
+                [
+                    "title" => "Classic Gray T-Shirt",
+                    "subtitle" => "100% Soft and Luxurious Cotton",
+                    "quantity" => 1,
+                    "price" => 25,
+                    "currency" => "USD",
+                    "image_url" => "http://petersapparel.parseapp.com/img/grayshirt.png"
                 ]
+            ],
+            "address" => [
+                "street_1" => "1 Hacker Way",
+                "street_2" => "",
+                "city" => "Menlo Park",
+                "postal_code" => "94025",
+                "state" => "CA",
+                "country" => "US"
             ],
             "summary" => [
                 "subtotal" => 75.00,
                 "shipping_cost" => 4.95,
                 "total_tax" => 6.19,
                 "total_cost" => 56.14
+            ],
+            "adjustments" => [
+                [
+                    "name" => "New Customer Discount",
+                    "amount" => 20
+                ],
+                [
+                    "name" => "$10 Off Coupon",
+                    "amount" => 10
+                ]
             ]
         ];
 });
