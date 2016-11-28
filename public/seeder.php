0d1c04c05b0a7b6c4a4a6d791de2d0ca211e24e7
Vu Tuan Anh <vu@Vus-MacBook-Pro-2.local>
2016-11-28 17:06:57 +0700

    commit

--- public/seeder.php
+++ public/seeder.php
@@ -229,13 +229,5 @@ $bot->answer('payload:USER_TAPPED_ABOUT',
 	'Elite Business Solutions (E-Biz) has been operating with the vision of provide ease for enterprise digitization in Asean countries. Check our website for more detail: http://ebiz.solutions'
 );
 
-
-
-// Action when user click "Get Started" button
-$bot->answer('payload:GIGA_GET_STARTED_PAYLOAD', 'Hi [first_name]! How are you today? Say \'Hello\' to begin the conversation');
-
-// Default answer
-$bot->answer('default:', 'Sorry I\'m not understand. You could check the Menu for begin the conversation. Thank you.');
-
 // Print some message to the browser when done
 dd('Nodes seeded!');
